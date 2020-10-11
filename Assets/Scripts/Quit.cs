ac4d6bb9887355562bc2558b5011a076018740b0
lolknight123 <LWaltersdorff@knights.ucf.edu>
2020-10-11 16:42:33 -0400

    First commit

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Quits the player when the user hits escape
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
